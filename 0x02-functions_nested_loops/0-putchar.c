1b7090303e3a91c2d918aebb607d443839f2d746
JOlindi <olindijosphat75@gmail.com>
2023-06-20 19:40:23 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,38 +1,27 @@
+/*
+ * File: 0-main.c
+ * Auth: Brennan D Baraban
+ *
+ */
+
 #include "main.h"
 
-	/**
-	* main - print _putchar
+/**
+  * main - Entry point
+  *
+  * Return: Always 0
+  */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
-	* is written
-	* Return: 0
-
-	*/
-
-
-
-	int main(void)
-
-        {
-
-        putchar('');
-
-        _putchar('p');
-
-        _putchar('u');
-
-        _putchar('t');
-
-        _putchar('c');
-
-        _putchar('h');
-
-        _putchar('a');
-
-        _putchar('r');
-
-        _putchar('\n');
-
-        return (0);
-
-        }
-
+	return (0);
+}
